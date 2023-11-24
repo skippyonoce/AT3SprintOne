aeac194d7982e882d457f77a6e1b105620479a75
DE JEAN FELIX <30083989@tafe.wa.edu.au>
2023-11-24 09:25:07 +0800

    Tooltips Added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.sortBtn = new System.Windows.Forms.Button();
             this.generateBtn = new System.Windows.Forms.Button();
@@ -47,78 +48,80 @@
             this.statsBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.SeqSearchBtn = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(16, 65);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(168, 452);
+            this.listBox1.Size = new System.Drawing.Size(127, 368);
             this.listBox1.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.listBox1, "This listbox will show the current state of \r\nthe data set loaded/stored in the p" +
+        "rogram.");
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // sortBtn
             // 
-            this.sortBtn.Location = new System.Drawing.Point(16, 15);
-            this.sortBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.sortBtn.Location = new System.Drawing.Point(12, 12);
             this.sortBtn.Name = "sortBtn";
-            this.sortBtn.Size = new System.Drawing.Size(169, 31);
+            this.sortBtn.Size = new System.Drawing.Size(127, 25);
             this.sortBtn.TabIndex = 3;
             this.sortBtn.Text = "SORT";
+            this.toolTip1.SetToolTip(this.sortBtn, "Click to Sort the data. (ascending order)");
             this.sortBtn.UseVisualStyleBackColor = true;
             this.sortBtn.Click += new System.EventHandler(this.sortBtn_Click);
             // 
             // generateBtn
             // 
-            this.generateBtn.Location = new System.Drawing.Point(16, 526);
-            this.generateBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.generateBtn.Location = new System.Drawing.Point(12, 427);
             this.generateBtn.Name = "generateBtn";
-            this.generateBtn.Size = new System.Drawing.Size(169, 37);
+            this.generateBtn.Size = new System.Drawing.Size(127, 30);
             this.generateBtn.TabIndex = 4;
             this.generateBtn.Text = "GENERATE";
+            this.toolTip1.SetToolTip(this.generateBtn, "Click to generate a new data set/list.\r\n");
             this.generateBtn.UseVisualStyleBackColor = true;
             this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 86);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(182, 70);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(231, 22);
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
             this.textBox1.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.textBox1, "Use this textfield to type in either the data you wish to search for \r\nOR to upda" +
+        "te/edit the highlighted list value to match the contents of this textfield.");
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(239, 66);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(179, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Input:";
             // 
             // editBtn
             // 
-            this.editBtn.Location = new System.Drawing.Point(413, 50);
-            this.editBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.editBtn.Location = new System.Drawing.Point(310, 41);
             this.editBtn.Name = "editBtn";
-            this.editBtn.Size = new System.Drawing.Size(61, 28);
+            this.editBtn.Size = new System.Drawing.Size(46, 23);
             this.editBtn.TabIndex = 6;
             this.editBtn.Text = "EDIT";
+            this.toolTip1.SetToolTip(this.editBtn, "Click this, after selecting the item you wish to edit in the list, \r\nand THEN cha" +
+        "nging the input textfield, \r\nto update/edit the item in the list.");
             this.editBtn.UseVisualStyleBackColor = true;
             this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
             // 
             // binarySearchBtn
             // 
-            this.binarySearchBtn.Location = new System.Drawing.Point(268, 114);
-            this.binarySearchBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.binarySearchBtn.Location = new System.Drawing.Point(201, 93);
             this.binarySearchBtn.Name = "binarySearchBtn";
-            this.binarySearchBtn.Size = new System.Drawing.Size(177, 28);
+            this.binarySearchBtn.Size = new System.Drawing.Size(133, 23);
             this.binarySearchBtn.TabIndex = 6;
             this.binarySearchBtn.Text = "BINARY SEARCH";
+            this.toolTip1.SetToolTip(this.binarySearchBtn, "Click to Binary Search for the the Input textfield value within the data list.");
             this.binarySearchBtn.UseVisualStyleBackColor = true;
             this.binarySearchBtn.Click += new System.EventHandler(this.binarySearchBtn_Click);
             // 
@@ -128,124 +131,122 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.Status});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 567);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 460);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(499, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
             this.statusStrip1.TabIndex = 7;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(56, 20);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(45, 17);
             this.toolStripStatusLabel1.Text = "Status: ";
             // 
             // Status
             // 
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(135, 20);
+            this.Status.Size = new System.Drawing.Size(106, 17);
             this.Status.Text = "Program Initialized";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 49);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(9, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "Hourly Neutrinos";
             // 
             // MidExBtn
             // 
-            this.MidExBtn.Location = new System.Drawing.Point(243, 314);
-            this.MidExBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.MidExBtn.Location = new System.Drawing.Point(182, 255);
             this.MidExBtn.Name = "MidExBtn";
-            this.MidExBtn.Size = new System.Drawing.Size(100, 28);
+            this.MidExBtn.Size = new System.Drawing.Size(75, 23);
             this.MidExBtn.TabIndex = 9;
             this.MidExBtn.Text = "Mid-Extreme";
+            this.toolTip1.SetToolTip(this.MidExBtn, "Click to calculate the Mid-Extreme of the data list.");
             this.MidExBtn.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 298);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(182, 242);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Statistics";
             // 
             // ModeBtn
             // 
-            this.ModeBtn.Location = new System.Drawing.Point(243, 340);
-            this.ModeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ModeBtn.Location = new System.Drawing.Point(182, 276);
             this.ModeBtn.Name = "ModeBtn";
-            this.ModeBtn.Size = new System.Drawing.Size(100, 28);
+            this.ModeBtn.Size = new System.Drawing.Size(75, 23);
             this.ModeBtn.TabIndex = 9;
             this.ModeBtn.Text = "Mode";
+            this.toolTip1.SetToolTip(this.ModeBtn, "Click to calculate the Mode of the data list.\r\n");
             this.ModeBtn.UseVisualStyleBackColor = true;
             this.ModeBtn.Click += new System.EventHandler(this.ModeBtn_Click);
             // 
             // AverageBtn
             // 
-            this.AverageBtn.Location = new System.Drawing.Point(243, 364);
-            this.AverageBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.AverageBtn.Location = new System.Drawing.Point(182, 296);
             this.AverageBtn.Name = "AverageBtn";
-            this.AverageBtn.Size = new System.Drawing.Size(100, 28);
+            this.AverageBtn.Size = new System.Drawing.Size(75, 23);
             this.AverageBtn.TabIndex = 9;
             this.AverageBtn.Text = "Average";
+            this.toolTip1.SetToolTip(this.AverageBtn, "Click to calculate the Average of the data list.\r\n");
             this.AverageBtn.UseVisualStyleBackColor = true;
             this.AverageBtn.Click += new System.EventHandler(this.AverageBtn_Click);
             // 
             // RangeBtn
             // 
-            this.RangeBtn.Location = new System.Drawing.Point(243, 389);
-            this.RangeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.RangeBtn.Location = new System.Drawing.Point(182, 316);
             this.RangeBtn.Name = "RangeBtn";
-            this.RangeBtn.Size = new System.Drawing.Size(100, 28);
+            this.RangeBtn.Size = new System.Drawing.Size(75, 23);
             this.RangeBtn.TabIndex = 9;
             this.RangeBtn.Text = "Range";
+            this.toolTip1.SetToolTip(this.RangeBtn, "Click to calculate the Range of the data list.\r\n");
             this.RangeBtn.UseVisualStyleBackColor = true;
             this.RangeBtn.Click += new System.EventHandler(this.RangeBtn_Click);
             // 
             // statsBox
             // 
-            this.statsBox.Location = new System.Drawing.Point(243, 449);
-            this.statsBox.Margin = new System.Windows.Forms.Padding(4);
+            this.statsBox.Location = new System.Drawing.Point(182, 365);
             this.statsBox.Multiline = true;
             this.statsBox.Name = "statsBox";
             this.statsBox.ReadOnly = true;
-            this.statsBox.Size = new System.Drawing.Size(201, 68);
+            this.statsBox.Size = new System.Drawing.Size(152, 56);
             this.statsBox.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.statsBox, "This textbox will display the statistical calculations\' results, \r\nafter having c" +
+        "licked one of the statistics buttons.");
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(239, 430);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(179, 349);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Stats Results:";
             // 
             // SeqSearchBtn
             // 
-            this.SeqSearchBtn.Location = new System.Drawing.Point(268, 145);
-            this.SeqSearchBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SeqSearchBtn.Location = new System.Drawing.Point(201, 118);
             this.SeqSearchBtn.Name = "SeqSearchBtn";
-            this.SeqSearchBtn.Size = new System.Drawing.Size(177, 28);
+            this.SeqSearchBtn.Size = new System.Drawing.Size(133, 23);
             this.SeqSearchBtn.TabIndex = 6;
             this.SeqSearchBtn.Text = "SEQUENTIAL SEARCH";
+            this.toolTip1.SetToolTip(this.SeqSearchBtn, "Click to Sequentially Search for the the Input textfield value within the data li" +
+        "st.");
             this.SeqSearchBtn.UseVisualStyleBackColor = true;
             this.SeqSearchBtn.Click += new System.EventHandler(this.SeqSearchBtn_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 593);
+            this.ClientSize = new System.Drawing.Size(374, 482);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.statsBox);
             this.Controls.Add(this.label3);
@@ -263,7 +264,6 @@
             this.Controls.Add(this.generateBtn);
             this.Controls.Add(this.sortBtn);
             this.Controls.Add(this.listBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Astronomical Processing";
             this.statusStrip1.ResumeLayout(false);
@@ -293,6 +293,7 @@
         private System.Windows.Forms.TextBox statsBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button SeqSearchBtn;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
